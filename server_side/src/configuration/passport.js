8f6ba6261960deed789de25334762cbe38db7a43
anantraj27 <anantraj5236@gmail.com>
2026-04-15 22:55:11 +0530

    jai shree ram

--- server_side/src/configuration/passport.js
+++ server_side/src/configuration/passport.js
@@ -1,69 +1,63 @@
-import passport from 'passport';
-import { Strategy } from 'passport-local'; ///  importing local
+import passport from "passport"
+import { Strategy } from "passport-local" ///  importing local
 
-import env from 'dotenv'; /// library for usieng process.env.  name
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
-dotenv.config();
-import  db  from './db.js';
+import env from "dotenv" /// library for usieng process.env.  name
+import bcrypt from "bcrypt"
+import dotenv from "dotenv"
+dotenv.config()
+import db from "./db.js"
 
 passport.use(
-    'local',
-    new Strategy(
-        {
-            /// by default  wo chhta hai kii body mej hamko , req.body.username and req.body.password  field hona chahiye , if nhi then btanna ka liya kii yeh khojo ... uska jagah
-            usernameField: 'Email', // 👈 YEH btana ka liya kii userb=name ka jagah yeh khojo  req body seh...
-            passwordField: 'password',
-        },
-        async function verify(Email, password, cb) {
-            try {
-                const result = await db.query('SELECT * FROM users WHERE  email = $1', [Email]);
-                if (result.rows.length === 0) {
-                    return cb(null, false, {
-                        message: 'Enter Email is not related to password',
-                    });
-                }
-
-                if (result.rows.length > 0) {
-                    const user = result.rows[0];
-                    const storedPassword = result.rows[0].password;
-
-                    bcrypt.compare(password, storedPassword, (err, ismatch) => {
-                        if (err) {
-                            return cb(err);
-                        } else {
-                            if (!ismatch) {
-                                return cb(null, false, {
-                                    message: 'Wrong password',
-                                });
-                            }
-                            if (ismatch) {
-                                return cb(null, user);
-                            } else {
-                                return cb(null, false);
-                            }
-                        }
-                    });
-                }
-            } catch (error) {
-                cb(error);
-            }
+  "local",
+  new Strategy(
+    {
+      /// by default  wo chhta hai kii body mej hamko , req.body.username and req.body.password  field hona chahiye , if nhi then btanna ka liya kii yeh khojo ... uska jagah
+      usernameField: "Email", // 👈 YEH btana ka liya kii userb=name ka jagah yeh khojo  req body seh...
+      passwordField: "password",
+    },
+    async function verify(Email, password, cb) {
+      try {
+        const result = await db.query("SELECT * FROM users WHERE  email = $1", [
+          Email,
+        ])
+        if (result.rows.length === 0) {
+          return cb(null, false, {
+            message: "Enter Email is not related to password",
+          })
         }
-    )
-);
 
+        if (result.rows.length > 0) {
+          const user = result.rows[0]
+          const storedPassword = result.rows[0].password
+
+          const ismatch = await bcrypt.compare(password, storedPassword)
+
+          if (!ismatch) {
+            return cb(null, false, {
+              message: "Wrong password",
+            })
+          }
+
+          return cb(null, user)
+        }
+      } catch (error) {
+        cb(error)
+      }
+    },
+  ),
+)
 
 passport.serializeUser((user, cb) => {
-    cb(null, user.id);
-});
+  cb(null, user.id)
+})
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+  try {
+    const result = await db.query("SELECT * FROM users WHERE id = $1", [id])
 
-        done(null, result.rows[0]);
-        // --> req.user = result.rows[0]
-    } catch (err) {
-        done(err, null);
-    }
-});
+    done(null, result.rows[0])
+    // --> req.user = result.rows[0]
+  } catch (err) {
+    done(err, null)
+  }
+})
