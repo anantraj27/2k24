ef2c76b7606f4b52b01505b0d005403344110138
anantraj27 <anantraj5236@gmail.com>
2026-04-15 20:06:04 +0530

    new feature

---
+++ public/login.js
@@ -0,0 +1,40 @@
+const API = "/api";
+login.addEventListener('click', async (e) => {
+
+  
+    try {
+        const  {data}  = await axios.get(
+            `${API}/auth/signin`,
+         
+            {
+                withCredentials: true
+            }
+        );
+       
+   
+        if (data.data.success) {
+           sessionStorage.setItem("username", data.name);
+            window.location.href = "home.html";
+        } else {
+            window.location.href ='login.html'
+            alert("Hey user 🤗 we couldn't find your account. Please log in. If you're new, go back 🔙 and register first.");
+        }
+
+    } catch (error) {
+            if (error.response) {
+        // Server responded with error status (4xx / 5xx)
+        console.log("Status:", error.response.status);
+        console.log("Data:", error.response.data);
+
+        alert(error.response.data.message || "Server error");
+
+    } else if (error.request) {
+        // Request sent but no response received
+        alert("Server not responding");
+
+    } else {
+        // Something wrong in frontend code
+        alert("Unexpected error: " + error.message);
+    }
+    }
+});
--- server_side/src/configuration/passport.js
+++ server_side/src/configuration/passport.js
@@ -17,7 +17,7 @@ passport.use(
         },
         async function verify(Email, password, cb) {
             try {
-                const result = await db.query('SELECT * FROM LOGIN WHERE  email = $1', [Email]);
+                const result = await db.query('SELECT * FROM users WHERE  email = $1', [Email]);
                 if (result.rows.length === 0) {
                     return cb(null, false, {
                         message: 'Enter Email is not related to password',
@@ -59,7 +59,7 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser(async (id, done) => {
     try {
-        const result = await db.query('SELECT * FROM login WHERE id = $1', [id]);
+        const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
 
         done(null, result.rows[0]);
         // --> req.user = result.rows[0]
