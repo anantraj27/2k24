39395182d729f6279af2b6ae8c36ef12bf6186fb
anantraj27 <anantraj5236@gmail.com>
2026-04-15 21:56:52 +0530

    verify

--- public/script.js
+++ public/script.js
@@ -149,44 +149,44 @@ clearInterval(countdown);
 
 const login = document.querySelector(".register-btn")
 const API = "/api";
-login.addEventListener('click', async (e) => {
+// login.addEventListener('click', async (e) => {
 
   
-    try {
-        const  {data}  = await axios.get(
-            `${API}/auth/checkAuth`,
+//     try {
+//         const  {data}  = await axios.get(
+//             `${API}/auth/checkAuth`,
     
          
-            {
-                withCredentials: true
-            }
-        );
+//             {
+//                 withCredentials: true
+//             }
+//         );
        
    
-        if (data.success) {
-           sessionStorage.setItem("username", data.name);
-            window.location.href = "home.html";
-        } else {
-            window.location.href ='login.html'
-            alert("Hey user 🤗 we couldn't find your account. Please log in. If you're new, go back 🔙 and register first.");
-        }
+//         if (data.success) {
+//            sessionStorage.setItem("username", data.name);
+//             window.location.href = "home.html";
+//         } else {
+//             window.location.href ='login.html'
+//             alert("Hey user 🤗 we couldn't find your account. Please log in. If you're new, go back 🔙 and register first.");
+//         }
 
-    } catch (error) {
-            if (error.response) {
-        // Server responded with error status (4xx / 5xx)
-        console.log("Status:", error.response.status);
-        console.log("Data:", error.response.data);
+//     } catch (error) {
+//             if (error.response) {
+//         // Server responded with error status (4xx / 5xx)
+//         console.log("Status:", error.response.status);
+//         console.log("Data:", error.response.data);
 
-        alert(error.response.data.message || "Server error");
+//         alert(error.response.data.message || "Server error");
 
-    } else if (error.request) {
-        // Request sent but no response received
-        alert("Server not responding");
+//     } else if (error.request) {
+//         // Request sent but no response received
+//         alert("Server not responding");
 
-    } else {
-        // Something wrong in frontend code
-        alert("Unexpected error: " + error.message);
-    }
-    }
-});
+//     } else {
+//         // Something wrong in frontend code
+//         alert("Unexpected error: " + error.message);
+//     }
+//     }
+// });
 
