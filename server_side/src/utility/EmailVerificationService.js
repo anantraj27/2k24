773521b67d12216e601898f159ce3fed6dafd246
anantraj27 <anantraj5236@gmail.com>
2026-06-23 15:55:08 +0530

    email-verification

--- server_side/src/utility/EmailVerificationService.js
+++ server_side/src/utility/EmailVerificationService.js
@@ -5,6 +5,7 @@ import nodemailer from "nodemailer"
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
+  family: 4,
   secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
   auth: {
     user: process.env.SMTP_USER,
