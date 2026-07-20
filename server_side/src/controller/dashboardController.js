accd5be196dd60cef62f0fe6ce68e6f6870cf36a
anantraj27 <anantraj5236@gmail.com>
2026-07-20 20:11:27 +0530

    initial commit

--- server_side/src/controller/dashboardController.js
+++ server_side/src/controller/dashboardController.js
@@ -401,10 +401,11 @@ export const eventRegister = async (req, res) => {
             ON tm.user_id = u.id
 
             WHERE
-                er.event_id = $1
+                er.event_id = $1,
+                AND er.participation_type = $2,
                 AND tm.user_id = ANY($2::int[])
             `,
-            [event_id, allMembers]
+            [event_id,participation_type, allMembers]
         );
 
         if (alreadyRegistered.length > 0) {
